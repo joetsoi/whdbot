98eede66de0c39a4c21f64cec8e6caeb1783b512
Joe Tsoi <joe.yeung.tsoi@gmail.com>
2012-10-29 17:03:20 +0000

    join multiple channels, catch all errors
    Only announcing death on URLErrors caused whdbot to stop checking
    Catch all exceptions and print to command line

--- whdbot.py
+++ whdbot.py
@@ -6,10 +6,11 @@ from urllib2 import urlopen, URLError, Request, HTTPCookieProcessor, build_opene
 from urllib import urlencode
 from httplib import BadStatusLine
 
-irclib.DEBUG = True
+#irclib.DEBUG = True
 network = 'uk.ircnet.org'
 port = 6667
 channel = '#qmul-its'
+channels = ['#freefrom', '#qmul-its', '#qmul']
 nick = 'whdbot'
 name  = 'whdbot'
 
@@ -21,7 +22,8 @@ class WhdBot():
 
         server = self.irc.server()
         server.connect(network, port, nick, ircname = name)
-        server.join(channel)
+        for channel in channels:
+            server.join(channel)
         self.connection = self.irc.connections[0]
 
         self.irc.execute_delayed(30, self.checkWhd)
@@ -30,16 +32,16 @@ class WhdBot():
     def handlePubMsg(self, connection, event):
         p = re.compile('.*?(\d{5}\d?).*?')
         m = p.match(event.arguments()[0])
-        print event.arguments()
+        #print event.arguments()
         if event.arguments()[0] == '!iswhddead':
             if self.whdDead:
                 print ">#whddead 'yes'"
-                connection.privmsg(channel, 'Yes')
+                connection.privmsg(event.target(), 'Yes')
             else:
                 print ">#whddead 'no'"
-                connection.privmsg(channel, 'No')
+                connection.privmsg(event.target(), 'No')
         elif m:
-            connection.privmsg(channel, "https://helpdesk.its.qmul.ac.uk/helpdesk/WebObjects/Helpdesk.woa/wa/ticket?ticketId={0}".format(m.group(1)))
+            connection.privmsg(event.target(), "https://helpdesk.its.qmul.ac.uk/helpdesk/WebObjects/Helpdesk.woa/wa/ticket?ticketId={0}".format(m.group(1)))
 
     def checkWhd(self):
         try:
@@ -50,22 +52,26 @@ class WhdBot():
             html = response.read()
             q = re.compile('.*?(https://helpdesk.its.qmul.ac.uk/helpdesk/WebObjects/Helpdesk.woa/wo/\w+/0.11.1.5.11).*?', re.DOTALL)
             n = q.match(html)
-            formData = urlencode({'userName':'test','password':'password'})
+            formData = urlencode({'userName':'username','password':'password'})
             post = opener.open(n.group(1), data=formData, timeout=20)
-            print post.read()
+            #print post.read()
 
             if self.whdDead:
                 self.whdDead = False
-                self.connection.privmsg(channel, 'It\'s back!')
+                for channel in channels:
+                    self.connection.privmsg(channel, 'It\'s back!')
             print 'it\'s alive'
-        except URLError:
-            print 'whd is dead'
+        except Exception as e:
+            print 'whd is dead', e
             if not self.whdDead:
-                self.connection.privmsg(channel, 'I think whd is dead')
+                for channel in channels:
+                    self.connection.privmsg(channel, 'I think whd is dead')
                 self.whdDead = True
-            self.connection.execute_delayed(10, self.checkWhd)
-        except SSLError,BadStatusLine:
-            pass
-        self.connection.execute_delayed(30, self.checkWhd)
+        finally:
+            self.connection.execute_delayed(30, self.checkWhd)
+
+
+    def autoOp(self, connection, event):
+        pass
 
 whdbot = WhdBot()
